aeee248e30c59b4729d1a4cc40b830164bf1da7f
SauloViana <sauloviana483@gmail.com>
2025-03-05 22:02:40 -0300

    chore: nome do projeto django alterado, app flowy_api criado, endpoint de teste

--- backend/flowy_core/urls.py
+++ backend/flowy_core/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for flowy_api project.
+URL configuration for flowy_core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from flowy_api.views import ping
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ping/', ping),
 ]
--- backend/flowy_core/wsgi.py
+++ backend/flowy_core/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for flowy_api project.
+WSGI config for flowy_core project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flowy_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flowy_core.settings')
 
 application = get_wsgi_application()
